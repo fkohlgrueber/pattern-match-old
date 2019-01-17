fc66156668c97d88153f6e6347b3c7bbc40968d3
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-01-17 15:37:49 +0100

    more changes

--- src/ast_match.rs
+++ src/ast_match.rs
@@ -1,6 +1,7 @@
-use crate::matchers::{IsMatch, MatchResult, Join};
+use crate::matchers::{IsMatch, MatchResult, Join, MatchSequences, MatchValues};
 use syntax::ast;
 use crate::pattern_tree::*;
+use itertools::Itertools;
 
 impl IsMatch<ast::TyKind> for Ty {
     fn is_match(&self, other: &ast::TyKind) -> Option<MatchResult> {
@@ -22,8 +23,8 @@ impl IsMatch<ast::ExprKind> for Expr {
     fn is_match(&self, other: &ast::ExprKind) -> Option<MatchResult> {
         match (self, other) {
             (Expr::Lit(i), ast::ExprKind::Lit(j)) => i.is_match(&j.node),
-            (Expr::Array(i), ast::ExprKind::Array(j)) => i.is_match(&j.iter().map(|x| &**x).collect::<Vec<_>>().as_slice()),
-            (Expr::Cast(ie, ity), ast::ExprKind::Cast(je, jty)) => ie.is_match(&je.node).join(ity.is_match(&jty.node)),
+            (Expr::Array(i), ast::ExprKind::Array(j)) => i.is_match_2(j),
+            (Expr::Cast(ie, ity), ast::ExprKind::Cast(je, jty)) => ie.is_match_2(je).join(ity.is_match(&jty.node)),
             (Expr::If(i_check, i_then, i_else), ast::ExprKind::If(j_check, j_then, j_else)) => 
                 i_check.is_match(&j_check.node)
                     .join(i_then.is_match(&j_then.stmts.iter().map(|x| x).collect::<Vec<_>>().as_slice()))
@@ -60,8 +61,8 @@ impl IsMatch<ast::LitKind> for Lit {
 impl IsMatch<ast::StmtKind> for Stmt {
     fn is_match(&self, other: &ast::StmtKind) -> Option<MatchResult> {
         match (self, other) {
-            (Stmt::Expr(i), ast::StmtKind::Expr(j)) => i.is_match(&j.node),
-            (Stmt::Semi(i), ast::StmtKind::Semi(j)) => i.is_match(&j.node),
+            (Stmt::Expr(i), ast::StmtKind::Expr(j)) => i.is_match_2(j),
+            (Stmt::Semi(i), ast::StmtKind::Semi(j)) => i.is_match_2(j),
             _ => None,
         }
     }
@@ -70,6 +71,52 @@ impl IsMatch<ast::StmtKind> for Stmt {
 impl IsMatch<ast::Stmt> for Stmt {
     fn is_match(&self, other: &ast::Stmt) -> Option<MatchResult> {
         self.is_match(&other.node)
+    }
+}
+
+pub trait IsMatch2<Rhs> {
+    fn is_match_2(&self, other: &Rhs) -> Option<MatchResult>;
+}
+
+impl<T, U> IsMatch2<syntax::source_map::Spanned<U>> for T
+where T: IsMatch<U> {
+    fn is_match_2(&self, other: &syntax::source_map::Spanned<U>) -> Option<MatchResult> {
+        self.is_match(&other.node)
+    }
+}
+
+impl<T, U> IsMatch2<syntax::ptr::P<U>> for T
+where T: IsMatch<U> {
+    fn is_match_2(&self, other: &syntax::ptr::P<U>) -> Option<MatchResult> {
+        self.is_match(&*other)
+    }
+}
+
+impl<T, U> IsMatch2<Vec<syntax::ptr::P<U>>> for MatchSequences<T>
+where T: IsMatch<U> {
+    fn is_match_2(&self, other: &Vec<syntax::ptr::P<U>>) -> Option<MatchResult> {
+        let iterators: Vec<_> = self.seq.iter().map(
+            |x| x.range.start..x.range.end.unwrap_or_else(|| other.len()+1)
+        ).multi_cartesian_product()
+         .filter(|x| x.iter().sum::<usize>() == other.len())
+         .collect();
+
+        'outer: for vals in iterators {
+            let mut skip = 0;
+            let mut res = Some(MatchResult::default());
+            for (i, v) in vals.iter().enumerate() {
+                res = other.iter().skip(skip).take(*v).fold(res, |r, x| {
+                    r.join(self.seq[i].elmt.is_match(x))
+                });
+                if res.is_none() {
+                    continue 'outer;
+                }
+                skip += v;
+            }
+            return res;
+        }
+        
+        None
     }
 }
 
