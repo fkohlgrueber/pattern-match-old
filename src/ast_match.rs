099050142cecc357043a2f9bc955c3a0590dc16c
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2019-02-25 10:43:14 +0100

    wip lifetime error

--- src/ast_match.rs
+++ src/ast_match.rs
@@ -16,78 +16,82 @@ impl pattern_tree::MatchAssociations for Ast {
     type Stmt = ast::Stmt;
 }
 
-impl<'cx, 'o, Cx> IsMatch<ast::LitKind> for Lit<'cx, 'o, Cx, Ast> {
-    fn is_match(&self, other: &ast::LitKind) -> bool {
+impl<'cx, 'o, Cx> IsMatch<'cx, 'o, Cx, ast::LitKind> for Lit<'cx, 'o, Cx, Ast> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::LitKind) -> (bool, &'cx mut Cx) {
         match (self, other) {
-            (Lit::Char(i), ast::LitKind::Char(j)) => i.is_match(j),
-            (Lit::Bool(i), ast::LitKind::Bool(j)) => i.is_match(j),
-            (Lit::Int(i), ast::LitKind::Int(j, _)) => i.is_match(j),
-            _ => false,
+            (Lit::Char(i), ast::LitKind::Char(j)) => i.is_match(cx, j),
+            (Lit::Bool(i), ast::LitKind::Bool(j)) => i.is_match(cx, j),
+            (Lit::Int(i), ast::LitKind::Int(j, _)) => i.is_match(cx, j),
+            _ => (false, cx),
         }
     }
 }
 
-impl<'cx, 'o, Cx> IsMatch<ast::ExprKind> for Expr<'cx, 'o, Cx, Ast> {
-    fn is_match(&self, other: &ast::ExprKind) -> bool {
+impl<'cx, 'o, Cx> IsMatch<'cx, 'o, Cx, ast::ExprKind> for Expr<'cx, 'o, Cx, Ast> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::ExprKind) -> (bool, &'cx mut Cx) {
         match (self, other) {
             (Expr::Lit(i), ast::ExprKind::Lit(j)) => 
-                i.is_match(j),
+                i.is_match(cx, j),
             (Expr::Block(i), ast::ExprKind::Block(j, _label)) => 
-                i.is_match(j),
+                i.is_match(cx, j),
             (Expr::Array(i), ast::ExprKind::Array(j)) => 
-                i.is_match(j),
-            (Expr::If(i_check, i_then, i_else), ast::ExprKind::If(j_check, j_then, j_else)) =>
-                i_check.is_match(j_check) && 
-                i_then.is_match(j_then) && 
-                i_else.is_match(j_else),
-            (Expr::IfLet(i_block, i_else), ast::ExprKind::IfLet(_pattern, _check, j_block, j_else)) => 
+                i.is_match(cx, j),
+            (Expr::If(i_check, i_then, i_else), ast::ExprKind::If(j_check, j_then, j_else)) => {
+                let (r_c, cx) = i_check.is_match(cx, j_check);
+                let (r_t, cx) = i_then.is_match(cx, j_then);
+                let (r_e, cx) = i_else.is_match(cx, j_else);
+                (r_c && r_t && r_e, cx)
+            },
+            (Expr::IfLet(i_block, i_else), ast::ExprKind::IfLet(_pattern, _check, j_block, j_else)) => {
                 // TODO: also check pattern and expr
-                i_block.is_match(j_block) && 
-                i_else.is_match(j_else),
-            _ => false,
+                let (r_b, cx) = i_block.is_match(cx, j_block);
+                let (r_e, cx) = i_else.is_match(cx, j_else);
+                (r_b && r_e, cx)
+            },
+            _ => (false, cx),
         }
     }
 }
 
-impl<'cx, 'o, Cx> IsMatch<ast::Expr> for Expr<'cx, 'o, Cx, Ast> {
-    fn is_match(&self, other: &ast::Expr) -> bool {
-        self.is_match(&other.node)
+impl<'cx, 'o, Cx> IsMatch<'cx, 'o, Cx, ast::Expr> for Expr<'cx, 'o, Cx, Ast> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::Expr) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &other.node)
     }
 }
 
-impl<'cx, 'o, Cx> IsMatch<ast::StmtKind> for Stmt<'cx, 'o, Cx, Ast> {
-    fn is_match(&self, other: &ast::StmtKind) -> bool {
+impl<'cx, 'o, Cx> IsMatch<'cx, 'o, Cx, ast::StmtKind> for Stmt<'cx, 'o, Cx, Ast> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::StmtKind) -> (bool, &'cx mut Cx) {
         match (self, other) {
-            (Stmt::Expr(i), ast::StmtKind::Expr(j)) => i.is_match(j),
-            (Stmt::Semi(i), ast::StmtKind::Semi(j)) => i.is_match(j),
-            _ => false,
+            (Stmt::Expr(i), ast::StmtKind::Expr(j)) => i.is_match(cx, j),
+            (Stmt::Semi(i), ast::StmtKind::Semi(j)) => i.is_match(cx, j),
+            _ => (false, cx),
         }
     }
 }
 
-impl<'cx, 'o, Cx> IsMatch<ast::Stmt> for Stmt<'cx, 'o, Cx, Ast> {
-    fn is_match(&self, other: &ast::Stmt) -> bool {
-        self.is_match(&other.node)
+impl<'cx, 'o, Cx> IsMatch<'cx, 'o, Cx, ast::Stmt> for Stmt<'cx, 'o, Cx, Ast> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::Stmt) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &other.node)
     }
 }
 
-impl<'cx, 'o, Cx, O> IsMatch<ast::Block> for Seq<'cx, 'o, Stmt<'cx, 'o, Cx, Ast>, Cx, O> {
-    fn is_match(&self, other: &ast::Block) -> bool {
-        self.is_match(&other.stmts)
+impl<'cx, 'o, Cx, O> IsMatch<'cx, 'o, Cx, ast::Block> for Seq<'cx, 'o, Stmt<'cx, 'o, Cx, Ast>, Cx, O> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o ast::Block) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &other.stmts)
     }
 }
 
-impl<T, U> IsMatch<syntax::ptr::P<U>> for T 
-where T: PatternTreeNode, T: IsMatch<U> {
-    fn is_match(&self, other: &syntax::ptr::P<U>) -> bool {
-        self.is_match(&*other)
+impl<'cx, 'o, Cx, T, U> IsMatch<'cx, 'o, Cx, syntax::ptr::P<U>> for T 
+where T: PatternTreeNode, T: IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o syntax::ptr::P<U>) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &*other)
     }
 }
 
-impl<T, U> IsMatch<syntax::source_map::Spanned<U>> for T 
-where T: PatternTreeNode, T: IsMatch<U> {
-    fn is_match(&self, other: &syntax::source_map::Spanned<U>) -> bool {
-        self.is_match(&other.node)
+impl<'cx, 'o, Cx, T, U> IsMatch<'cx, 'o, Cx, syntax::source_map::Spanned<U>> for T 
+where T: PatternTreeNode, T: IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o syntax::source_map::Spanned<U>) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &other.node)
     }
 }
 
--- src/matchers.rs
+++ src/matchers.rs
@@ -16,48 +16,59 @@ impl IsMatchEquality for char {}
 impl IsMatchEquality for bool {}
 
 // Main trait for matching
-pub trait IsMatch<T> {
-    fn is_match(&self, other: &T) -> bool;
+pub trait IsMatch<'cx, 'o, Cx, O> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o O) -> (bool, &'cx mut Cx);
 }
 
 // Trait for types that can be matched by their equality
 pub trait IsMatchEquality: PartialEq {}
 
-impl<T> IsMatch<T> for T 
+impl<'cx, 'o, Cx, T> IsMatch<'cx, 'o, Cx, T> for T 
 where T: IsMatchEquality {
-    fn is_match(&self, other: &T) -> bool {
-        self == other
+    fn is_match(&self, cx: &'cx mut Cx, other: &T) -> (bool, &'cx mut Cx) {
+        (self == other, cx)
     }
 }
 
-impl<'cx, 'o, T, U, Cx, O> IsMatch<U> for Alt<'cx, 'o, T, Cx, O>
-where T: PatternTreeNode + IsMatch<U> {
-    fn is_match(&self, other: &U) -> bool {
+impl<'cx, 'o, T, U, Cx, O> IsMatch<'cx, 'o, Cx, U> for Alt<'cx, 'o, T, Cx, O>
+where T: PatternTreeNode + IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o U) -> (bool, &'cx mut Cx) {
         match self {
-            Alt::Any => true,
-            Alt::Elmt(e) => e.is_match(other),
-            Alt::Named(e, _) => e.is_match(other),
-            Alt::Alt(a, b) => a.is_match(other) || b.is_match(other)
+            Alt::Any => (true, cx),
+            Alt::Elmt(e) => e.is_match(cx, other),
+            Alt::Named(e, _) => e.is_match(cx, other),
+            Alt::Alt(i, j) => {
+                let (r_i, cx) = i.is_match(cx, other);
+                let (r_j, cx) = j.is_match(cx, other);
+                (r_i || r_j, cx)
+            }
         }
     }
 }
 
-impl<'cx, 'o, T, U, Cx, O> IsMatch<&[U]> for Seq<'cx, 'o, T, Cx, O>
-where T: PatternTreeNode + IsMatch<U> {
-    fn is_match(&self, other: &&[U]) -> bool {
-        
+impl<'cx, 'o, T, U, Cx, O> IsMatch<'cx, 'o, Cx, &[U]> for Seq<'cx, 'o, T, Cx, O>
+where T: PatternTreeNode + IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o &[U]) -> (bool, &'cx mut Cx) {
+        let mut cx = cx;
         match self {
-            Seq::Any => other.len() == 1,
-            Seq::Elmt(e) => other.len() == 1 && e.is_match(&other[0]),
-            Seq::Named(e, _) => e.is_match(other),
-            Seq::Alt(a, b) => a.is_match(other) || b.is_match(other),
-            Seq::Empty => other.is_empty(),
+            Seq::Any => (other.len() == 1, cx),
+            Seq::Elmt(e) => {
+                let (r_e, cx) = e.is_match(cx, &other[0]);
+                (other.len() == 1 && r_e, cx)
+            },
+            Seq::Named(e, _) => e.is_match(cx, other),
+            Seq::Alt(i, j) => {
+                let (r_i, cx) = i.is_match(cx, other);
+                let (r_j, cx) = j.is_match(cx, other);
+                (r_i || r_j, cx)
+            },
+            Seq::Empty => (other.is_empty(), cx),
             Seq::Repeat(e, r) => {
                 let e_range = e.num_elmts_range();
                 let e_range = e_range.start..e_range.end.unwrap_or(other.len()+1);
 
                 if r.start == 0 && other.is_empty() {
-                    return true;
+                    return (true, cx);
                 }
 
                 for i in r.start..r.end.unwrap_or(other.len()+1) {
@@ -70,56 +81,67 @@ where T: PatternTreeNode + IsMatch<U> {
                         let mut skip = 0;
                         for v in vals.iter() {
                             
-                            if !e.is_match(&&other[skip..skip+v]) {
+                            let (r_e, cx_tmp) = e.is_match(cx, &&other[skip..skip+v]);
+                            cx = cx_tmp;
+                            if !r_e {
                                 continue 'outer;
                             }
                             skip += v;
                         }
-                        return true;
+                        return (true, cx);
                     }
                 }
 
-                false
+                (false, cx)
             },
             Seq::Seq(a, b) => {
+                let mut cx = cx;
                 let range = a.num_elmts_range();
                 for i in range.start..range.end.unwrap_or(other.len()+1) {
                     if i > other.len() {
                         break;
                     }
                     let (l, r) = other.split_at(i);
-                    if a.is_match(&l) && b.is_match(&r) {
-                        return true;
+                    let (r_a, cx_tmp) = a.is_match(cx, &l);
+                    cx = cx_tmp;
+                    let (r_b, cx_tmp) = b.is_match(cx, &r);
+                    cx = cx_tmp;
+                    if r_a && r_b {
+                        return (true, cx);
                     }
                 }
-                false
+                (false, cx)
             },
             
         }
     }
 }
 
-impl<'cx, 'o, T, U, Cx, O> IsMatch<Vec<U>> for Seq<'cx, 'o, T, Cx, O>
-where T: PatternTreeNode + IsMatch<U> {
-    fn is_match(&self, other: &Vec<U>) -> bool {
-        self.is_match(&other.as_slice())
+impl<'cx, 'o, T, U, Cx, O> IsMatch<'cx, 'o, Cx, Vec<U>> for Seq<'cx, 'o, T, Cx, O>
+where T: PatternTreeNode + IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o Vec<U>) -> (bool, &'cx mut Cx) {
+        self.is_match(cx, &other.as_slice())
     }
 }
 
 
-impl<'cx, 'o, T, U, Cx, O> IsMatch<Option<U>> for Opt<'cx, 'o, T, Cx, O>
-where T: PatternTreeNode + IsMatch<U> {
-    fn is_match(&self, other: &Option<U>) -> bool {
+impl<'cx, 'o, T, U, Cx, O> IsMatch<'cx, 'o, Cx, Option<U>> for Opt<'cx, 'o, T, Cx, O>
+where T: PatternTreeNode + IsMatch<'cx, 'o, Cx, U> {
+    fn is_match(&self, cx: &'cx mut Cx, other: &'o Option<U>) -> (bool, &'cx mut Cx) {
         
         match self {
-            Opt::Any => other.is_some(),
+            Opt::Any => (other.is_some(), cx),
             Opt::Elmt(e) => match other {
-                Some(other) => e.is_match(other),
-                None => false
+                Some(other) => e.is_match(cx, other),
+                None => (false, cx)
             },
-            Opt::Named(e, _) => e.is_match(other),
-            Opt::Alt(a, b) => a.is_match(other) || b.is_match(other),
-            Opt::None => other.is_none(),
+            Opt::Named(e, _) => e.is_match(cx, other),
+            Opt::Alt(a, b) => {
+                let (r_a, cx) = a.is_match(cx, other);
+                let (r_b, cx) = b.is_match(cx, other);
+                (r_a && r_b, cx)
+            },
+            Opt::None => (other.is_none(), cx),
         }
     }
 }
